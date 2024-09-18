36cf105d9e54dfe51f582320b9e97ec460e88a96
Manuel Ramos ⬇️ SrPatoMan 🦆 <85370678+SrPatoMan@users.noreply.github.com>
2024-09-18 03:22:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -268,7 +268,7 @@ mkdir -p ~/.config/bspwm ~/.config/sxhkd
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 chmod +x ~/.config/bspwm/bspwmrc
-
+echo "exec bspwm" >> ~/.xinitrc
 
 
 ## Configuracion Kitty ##
