6783dba3cadeb3e2c220fbca99878246d7567e98
Manuel Ramos ⬇️ SrPatoMan 🦆 <85370678+SrPatoMan@users.noreply.github.com>
2024-09-27 17:25:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -261,13 +261,14 @@ ruta_wordlist2=$HOME/wordlists/discovery
 ruta_wordlist3=$HOME/wordlists/payloads/sqli
 ruta_wordlist4=$HOME/wordlists/payloads/lfi
 ruta_wordlist5=$HOME/wordlists/payloads/redirect
+ruta_wordlist6=$HOME/wordlists/file_leaks
 
 mkdir -p $ruta_wordlist1
 mkdir -p $ruta_wordlist2
 mkdir -p $ruta_wordlist3
 mkdir -p $ruta_wordlist4
 mkdir -p $ruta_wordlist5
-
+mkdir -p $ruta_wordlist6
 
 
 sudo mv HackerMachine/xss_cheat_sheet/* $ruta_wordlist1
@@ -287,13 +288,18 @@ wget 'https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Conte
 wget 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/CMS/wp-plugins.fuzz.txt'
 wget 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-medium.txt'
 wget 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-big.txt'
-wget 'https://raw.githubusercontent.com/SrPatoMan/bbtips/refs/heads/main/bbtips.txt'
+wget 'https://raw.githubusercontent.com/SrPatoMan/bbtips/refs/heads/main/wordlists/bbtips.txt'
+wget 'https://raw.githubusercontent.com/dkcyberz/Harpy/refs/heads/main/Hidden/database.txt'
 sudo mv big-list-of-naughty-strings.txt $ruta_wordlist2
 sudo mv wordpress.fuzz.txt $ruta_wordlist2
 sudo mv wp-plugins.fuzz.txt $ruta_wordlist2
 sudo mv directory-list-2.3-medium.txt $ruta_wordlist2
 sudo mv directory-list-2.3-big.txt $ruta_wordlist2
-sudo mv bbtips.txt $ruta_wordlist2
+sudo mv bbtips.txt $ruta_wordlist6
+sudo mv database.txt $ruta_wordlist6
+sudo mv zipfiles.txt $ruta_wordlist6
+git clone https://github.com/0xPugal/fuzz4bounty
+sudo mv fuzz4bounty/fuzz4bounty $HOME/wordlists/file_leaks
 
 ## Configurando Firefox ##
 
